174afaca08d1eed3e4be67a438e68634bb10c369
David Cai <57420559+david-cai@users.noreply.github.com>
2020-05-04 19:07:09 -0400

    Add rigidBody component

---
+++ Rocket Game/Assets/Rocket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start() {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ProcessInput();
+    }
+
+    private void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)) {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+
+        }
+    }
+
+}
