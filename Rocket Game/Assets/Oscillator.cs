af948de309aa55d6aa6902cd166fc5203502bc9b
David Cai <57420559+david-cai@users.noreply.github.com>
2020-05-08 14:52:18 -0400

    Adding object movement

--- Rocket Game/Assets/Oscillator.cs
+++ Rocket Game/Assets/Oscillator.cs
@@ -7,13 +7,16 @@ public class Oscillator : MonoBehaviour {
     [SerializeField] Vector3 movementVector;
 
     [Range(0,1)][SerializeField] float movementFactor;
+
+    Vector3 startingPos;
     // Start is called before the first frame update
     void Start() {
-
+        startingPos = transform.position;
     }
 
     // Update is called once per frame
     void Update() {
-
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
     }
 }
